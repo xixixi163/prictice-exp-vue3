110ea791270b5850220afee020b560ce632d5b94
xixixi163 <68846852+xixixi163@users.noreply.github.com>
2023-05-06 14:57:49 +0800

    feat: 添加状态

--- src/utils/promise/promise.js
+++ src/utils/promise/promise.js
@@ -1,40 +1,74 @@
 
-// 设置状态保证resolve 只调用一次。pending时可调用。
-const pending = "pending";
-const fulfilled = "fulfilled"
+const pending = 'pending';
+const fulfilled = 'fulfilled';
 
-// 发布者 resolve 订阅者 then；发布者通知订阅者
 export default class MyPromise {
-    constructor(run) { // run 函数(resolve)=>any
-        this.observerList = [];
+    constructor(run) {
+        // resolve 闭包，创建promise时 发布，then 是订阅
+        // then 内部 调用
+        this.resolvedCallback = [];
+        this.data = void 666; // 存异步的结果,也就是settime out 的结果
         this.status = pending;
-        
-        // 发布者事件。对用户订阅的事件执行
         const resolve = (value) => {
-            // resolve 要保证只能调用依次，所以设置状态
-            if (this.status === pending) {
-                this.status = fulfilled;
-                this.observerList.forEach(callback => callback(value))
-                console.log(this.observerList, 'resolve');
+            if(this.status === pending) {
+                this.status = fulfilled
+                this.data = value
+                this.resolvedCallback.forEach(callback => {
+                    console.log(callback, 'resolve--------');
+                    callback(this.data)
+                })
             }
-        };
-        run(resolve); // 执行resolve 发布者;在resolve()时调用了
+        }
+        run(resolve) // 执行传进来的函数 并且传递闭包，使在run内部调用
     }
-    // 订阅，把需要订阅的事件放入观察者列表
-    then(callback) {
-        this.observerList.push(callback)
+    then(onResolved) {
+        // 链式调用，那么需要返回promise 才可以继续使用class 中的then
+        // pending 状态，继续订阅 push；因为pending状态 可以发布订阅
+        // fulfilled 状态，直接返回新promise；返回上一次promise 对象的结果给新的promise对象
+        switch (this.status) {
+            case pending: {
+                return new MyPromise((resolve) => {
+                    this.resolvedCallback.push(value => { // 再包一层
+                        // 判断then传入的回调返回的类型
+                        const result = onResolved(value)
+                        console.log("pending: ", result);
+                        if (result instanceof MyPromise) {
+                            // 如果是promise 可以直接调用then，
+                            result.then(resolve)
+                        } else {
+                            resolve(result) // 执行闭包
+                        }
+                    })
+                })
+            }
+                
+            
+            case fulfilled: {
+                return new MyPromise(resolve => {
+                    const result = onResolved(this.data) // 上一次pending的结果
+                    console.log("fulfilled: ", result);
+                    if(result instanceof MyPromise) {
+                        result.then(resolve) // 订阅
+                    } else {
+                        resolve(result) // 执行闭包 发布逻辑
+                    }
+                })
+            }
+
+            default:
+                break;
+        }
     }
 }
 
 export const testPromise = () => {
     const p = new MyPromise((resolve) => {
-        // resolve调用需要在异步任务中，这样执行顺序就为 then ——> resolve ，即订阅的消息进入池子，发布者进行发布。
-        // 为什么顺序是这样，一进来加载script（mro），然后执行所有mri，在走mro，这时也是就settimeout
         setTimeout(() => {
-            // resolve 要保证只能调用一次，所以设置状态
-            resolve('settime out')
-            resolve('hello')
+            resolve('set time out')  // 执行闭包，还要保证只执行一次
+            resolve('other--')
         })
-      });
-    p.then(data => console.log(data))  // then
+    })
+    p.then(data => data + '8989')
+    .then(data => data.toUpperCase())
+    .then(console.log) // 订阅，
 }
