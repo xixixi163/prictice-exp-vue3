4ce112379b01ea35b463c0664af7b045f0ed1b24
xixixi163 <68846852+xixixi163@users.noreply.github.com>
2023-07-28 18:40:37 +0800

    feat: scroll

--- src/views/virtual-scroll/components/virtual-accordant/index.vue
+++ src/views/virtual-scroll/components/virtual-accordant/index.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="virtual-list-container">
+        <!-- 虚拟列表的高度 -->
         <div 
             class="virtual-list-body-wrapper body-wrapper"
-            style=""
+            :style="{maxHeight: `${listHeight}px`}"
             @scroll="handleScroll"
         >
-            <div class="virtual-body-y-space" style="">
-                <div class="list-area" style="">
-                    <div class="list-item">
-                        <slot>
-                            index
-                        </slot>
-                    </div>
+        <!-- 滚动区域占位 -->
+            <div class="virtual-body-y-space" :style="{height: `${listData.length * lineHeight}px`}"></div>
+            <div class="list-area" style="">
+                <div v-for="(item, index) in realList" :key="item.key" class="list-item">
+                    <slot>
+                        {{ item }}---{{ index }}
+                    </slot>
                 </div>
             </div>
         </div>
@@ -21,22 +22,46 @@
 export default {
     name: 'VirtualAccordant',
     props: {
+        // 无限数组
         listData: {
             type: Array,
             default: () => []
         },
+        // 列表高度
         listHeight: {
             type: Number,
             default: 420
         },
+        // 列表项高度
         lineHeight: {
             type: Number,
             default: 40
+        },
+        // 一屏渲染个数
+        renderChildNum: {
+            type: Number,
+            default: 10
+        }
+    },
+    data() {
+        return {
+            start: 0, // 滚动开始的位置
+            scrollTop: 0, // 向上滚动的高度
+        }
+    },  
+    computed: {
+        // 获取一屏的数据
+        realList() {
+            return this.listData.slice(this.start, this.start + this.renderChildNum)
+        },
+        // 顶部预留位置
+        marginTop() {
+            return this.start * this.lineHeight
         }
     },
     methods: {
-        handleScroll() {
-
+        handleScroll(e) {
+            this.scrollTop = e.target.scrollTop;
         }
     }
 }
--- src/views/virtual-scroll/index.vue
+++ src/views/virtual-scroll/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div>virtual
-        <virtual-accordant></virtual-accordant>
+        <virtual-accordant
+            :list-data="listData"
+            :list-height="400"
+            :line-height="40"
+            :render-child-num="10">
+
+        </virtual-accordant>
     </div>
 </template>
 <script>
@@ -9,6 +15,22 @@ export default {
     components: {
         VirtualAccordant
     },
+    data() {
+        return {
+            listData: [{
+                key: 0,
+                value: 1
+            }, {
+                key: 0,
+                value: 2
+            }]
+        }
+    },
+    methods: {
+        getListData() {
+            
+        }
+    }
 }
 </script>
 <style scoped>
